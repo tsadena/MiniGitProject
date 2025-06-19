6ca5a1e435e20c156e862924d0cfde5dc9c4f07f
tsadena <tsediab11@gmail.com>
2025-06-19 22:47:32 +0300

    Initial Commit

---
+++ MiniGitProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MiniGitProject
+//
+//  Created by Tsadena :) on 6/19/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
